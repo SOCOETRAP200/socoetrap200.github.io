22c1a17c14dac898e5f461302408940cb4a7a208
SOCOETRAP200 <cubahirogordie@gmail.com>
2026-06-30 14:06:42 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,58 +1,57 @@
+async function loadPrices() {
 
-    const balance = document.getElementById("balance");
-const refreshBtn = document.getElementById("refreshBtn");
-const coinList = document.getElementById("coin-list");
+const url =
+"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana,ripple,tether,pi-network&vs_currencies=usd";
 
-let totalBalance = 15234.75;
+try {
+
+const response = await fetch(url);
+
+const data = await response.json();
 
 const coins = [
-  { name: "Pi Network", symbol: "PI", price: 0.75, amount: 1000 },
-  { name: "Bitcoin", symbol: "BTC", price: 105000, amount: 0.05 },
-  { name: "Ethereum", symbol: "ETH", price: 3500, amount: 1.2 },
-  { name: "BNB", symbol: "BNB", price: 720, amount: 2 },
-  { name: "Solana", symbol: "SOL", price: 185, amount: 15 },
-  { name: "XRP", symbol: "XRP", price: 2.15, amount: 500 },
-  { name: "USDT", symbol: "USDT", price: 1.00, amount: 1000 }
+
+{name:"Pi Network",symbol:"PI",price:data["pi-network"]?.usd ?? "N/A"},
+
+{name:"Bitcoin",symbol:"BTC",price:data.bitcoin.usd},
+
+{name:"Ethereum",symbol:"ETH",price:data.ethereum.usd},
+
+{name:"BNB",symbol:"BNB",price:data.binancecoin.usd},
+
+{name:"Solana",symbol:"SOL",price:data.solana.usd},
+
+{name:"XRP",symbol:"XRP",price:data.ripple.usd},
+
+{name:"USDT",symbol:"USDT",price:data.tether.usd}
+
 ];
 
-function updateBalance() {
-  balance.textContent = "$" + totalBalance.toLocaleString();
-}
+coinList.innerHTML="";
 
-function displayCoins() {
-  coinList.innerHTML = "";
+coins.forEach((coin)=>{
 
-  coins.forEach((coin) => {
-    const item = document.createElement("div");
-    item.className = "coin";
+coinList.innerHTML+=`
+<div class="coin">
+<div>
+<div class="coin-name">${coin.name}</div>
+<small>${coin.symbol}</small>
+</div>
 
-    item.innerHTML = `
-      <div>
-        <div class="coin-name">${coin.name}</div>
-        <small>${coin.amount} ${coin.symbol}</small>
-      </div>
+<div class="coin-price">
+$${coin.price}
+</div>
+</div>
+`;
 
-      <div class="coin-price">
-        $${coin.price.toLocaleString()}
-      </div>
-    `;
-
-    coinList.appendChild(item);
-  });
-}
-
-refreshBtn.addEventListener("click", () => {
-  totalBalance += Math.random() * 100 - 50;
-  totalBalance = Number(totalBalance.toFixed(2));
-
-  coins.forEach((coin) => {
-    const variation = (Math.random() * 0.06) - 0.03;
-    coin.price = Number((coin.price * (1 + variation)).toFixed(2));
-  });
-
-  updateBalance();
-  displayCoins();
 });
 
-updateBalance();
-displayCoins();
+}catch(error){
+
+console.log(error);
+
+}
+
+}
+
+loadPrices();
